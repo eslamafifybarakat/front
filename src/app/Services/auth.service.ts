058f2c583d0936810ce86bfbe367204628e459cb
baselllll <baselosama185@yahoo.com>
2021-07-12 03:10:53 +0200

    ss

---
+++ src/app/Services/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import jwtDecode from 'jwt-decode';
+import { Router } from '@angular/router';
+import { Observable,BehaviorSubject } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  currentUser = new BehaviorSubject(null);
+  
+  constructor(private _HttpClient:HttpClient, private _Router:Router) { 
+    if(localStorage.getItem('userToken') !=null){
+      this.saveCurrentUser();
+
+    }
+  
+
+  }
+
+  saveCurrentUser()
+  {
+    let token:any=localStorage.getItem('userToken');
+    this.currentUser.next(jwtDecode(token));
+    console.log(this.currentUser);
+  } 
+
+
+  login(formData:any):Observable<any>{
+    return this._HttpClient.post('http://localhost:3333/api/user/login',formData);
+  }
+  register(formData:any):Observable<any>{
+    return this._HttpClient.post('http://localhost:3333/api/user/add-user',formData);
+  }
+  logout(){
+    this.currentUser.next(null);
+    localStorage.removeItem('userToken');
+    this._Router.navigate(['./sign-in']);
+
+  }
+}
