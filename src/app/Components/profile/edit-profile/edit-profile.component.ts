5e4e89a46077c5a82185b61ff632a7fa6afae42b
Abd El Rahman Osama <aerosama793@gmail.com>
2021-07-17 03:45:39 +0200

    added send email service

--- src/app/Components/profile/edit-profile/edit-profile.component.ts
+++ src/app/Components/profile/edit-profile/edit-profile.component.ts
@@ -6,32 +6,66 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
-  styleUrls: ['./edit-profile.component.scss']
+  styleUrls: ['./edit-profile.component.scss'],
 })
 export class EditProfileComponent implements OnInit {
-  email:any= localStorage.getItem('email');
-  id:any= localStorage.getItem('id');
-  street:any= localStorage.getItem('street');
-  city:any= localStorage.getItem('city');
-  phone:any= localStorage.getItem('phone');
-  name:any= localStorage.getItem('name');
-  constructor(private myuser:AuthService,private myrouter:Router,private _AuthService:AuthService) { }
+  email: any = localStorage.getItem('email');
+  id: any = localStorage.getItem('id');
+  street: any = localStorage.getItem('street');
+  city: any = localStorage.getItem('city');
+  phone: any = localStorage.getItem('phone');
+  name: any = localStorage.getItem('name');
+  constructor(
+    private myuser: AuthService,
+    private myrouter: Router,
+    private _AuthService: AuthService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  editprofile(name:any,phone:any,email:any,street:any,city:any,password:any){
+  editprofile(
+    name: any,
+    phone: any,
+    email: any,
+    street: any,
+    city: any,
+    password: any
+  ) {
     const salt = bcrypt.genSaltSync(10);
     const newpass = bcrypt.hashSync(password, salt);
-    let data={
-      "Name":name,
-      "Email":email,
-      "Password":newpass,
-      "Phone":phone,
-      "Address_street":street,
-      "Address_city":city
-    }
-    this.myuser.updateUser(this.id,data).subscribe(res=>{console.log(res)},err=>{console.log(err)})
+    let data = {
+      Name: name,
+      Email: email,
+      Password: newpass,
+      Phone: phone,
+      Address_street: street,
+      Address_city: city,
+    };
+    this.myuser.updateUser(this.id, data).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+
+
+    this._AuthService
+      .sendPasswordChangeMail({
+        email: email,
+        password: password,
+      })
+      .subscribe(
+        (res) => {
+          console.log(res);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+
+      
     this._AuthService.logout();
   }
 }
--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -2,45 +2,52 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import jwtDecode from 'jwt-decode';
 import { Router } from '@angular/router';
-import { Observable,BehaviorSubject } from 'rxjs';
-
-
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   currentUser = new BehaviorSubject(null);
-  
-  constructor(private _HttpClient:HttpClient, private _Router:Router) { 
-    if(localStorage.getItem('userToken') !=null){
-      this.saveCurrentUser();
 
+  constructor(private _HttpClient: HttpClient, private _Router: Router) {
+    if (localStorage.getItem('userToken') != null) {
+      this.saveCurrentUser();
     }
   }
 
-  saveCurrentUser()
-  {
-    let token:any=localStorage.getItem('userToken');
+  saveCurrentUser() {
+    let token: any = localStorage.getItem('userToken');
     this.currentUser.next(jwtDecode(token));
     console.log(this.currentUser);
-  } 
-
-
-  login(formData:any):Observable<any>{
-    return this._HttpClient.post('http://localhost:3333/api/user/login',formData);
   }
-  register(formData:any):Observable<any>{
-    return this._HttpClient.post('http://localhost:3333/api/user/add-user',formData);
+
+  login(formData: any): Observable<any> {
+    return this._HttpClient.post(
+      'http://localhost:3333/api/user/login',
+      formData
+    );
   }
-  logout(){
+  register(formData: any): Observable<any> {
+    return this._HttpClient.post(
+      'http://localhost:3333/api/user/add-user',
+      formData
+    );
+  }
+  logout() {
     this.currentUser.next(null);
-    localStorage.clear()
+    localStorage.clear();
     this._Router.navigate(['./sign-in']);
   }
-  updateUser(id:any,data:any){
-    return this._HttpClient.put(`http://localhost:3333/api/user/update-user/${id}`,data);
+  updateUser(id: any, data: any) {
+    return this._HttpClient.put(
+      `http://localhost:3333/api/user/update-user/${id}`,
+      data
+    );
+  }
+
+  sendPasswordChangeMail(data: any) {
+    return this._HttpClient.post(`http://localhost:3333/sendMail`, data);
   }
   deleteUser(id: any) {
     return this._HttpClient.delete(
